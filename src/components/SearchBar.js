b3c409f7d28b5535183171e2ab68f8a3243ea88f
QuintyHH <razmocanu@yahoo.com>
2019-07-12 04:44:43 +0300

    fixed name and added gh-pages

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -13,7 +13,7 @@ onFormSubmit = e => {
       <div className="ui segment">
         <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
-            <label>Image Search</label>
+            <label>Not-The-Smartest Image Search</label>
             <input 
               type="text" 
               value={this.state.term} 
